038b256e742502a7bcad785add6aaeb736814f7b
andrewtong <andrewtong19@gmail.com>
2015-02-22 16:49:55 -0800

    Update SearchListings.java

--- SearchListings.java
+++ SearchListings.java
@@ -1,10 +1,6 @@
-package smartBuyer;
+package at.smartBuyer.searchlistings;
 
 import java.util.List;
-
-
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 
 import com.ebay.services.client.ClientConfig;
 import com.ebay.services.client.FindingServiceClientFactory;
@@ -15,49 +11,53 @@ import com.ebay.services.finding.FindingServicePortType;
 import com.ebay.services.finding.ItemFilter;
 import com.ebay.services.finding.ItemFilterType;
 import com.ebay.services.finding.PaginationInput;
-import com.ebay.services.finding.SearchItem;
 import com.ebay.services.finding.SortOrderType;
 
-import detailAbstraction.SearchDetails;
-import sqlCommunication.StoreInfo;
-
 public class SearchListings {
-
-    public static void main(String[] args) {
-    	
-        try {
-        	// Setup endpoint configuration
+	
+	public static final int searchentries = 100;
+	public static final String appid = "User App Id";
+	
+	public static FindItemsAdvancedResponse searchListings(){
+		try {
+        	// Setup Endpoint Configuration
         	ClientConfig config = new ClientConfig();
-        	config.setHttpHeaderLoggingEnabled(false);//removes initial collection log
-        	config.setSoapMessageLoggingEnabled(false); //removes Soap Message log for all items
-        	config.setApplicationId("User Application ID");
+        	
+        	//Setting this value as false suppresses the console SOAP messages and the XML input/output associated
+        	//with the user search.
+        	config.setHttpHeaderLoggingEnabled(false);
+        	config.setSoapMessageLoggingEnabled(false);
+        	//An application id is obtained by signing up for the EBay developer's program
+        	config.setApplicationId(appid);
+        	//
 
-        	//Creates the service client
+        	//Creates a Service Client
+        	//A service client accepts a user request and returns a result based off user input
             FindingServicePortType serviceClient = FindingServiceClientFactory.getServiceClient(config);
+            //
             
             //Request Object
+            //A number of parameters can be added to the request object to specify the user desired search listings
             FindItemsAdvancedRequest request = new FindItemsAdvancedRequest();
-            //
-            
-            StoreInfo.establishConnection();
-            StoreInfo.lookupListingsTable();
-            StoreInfo.lookupComparisonTable();
-            
-            
+
             //Request Parameters
-            //request.setKeywords("Product Keywords");
-            //
             
-            //Select Category of Item, supports up to 3 categories per input call
-            //int of the form "int" to denote category ID
+            //Keywords - product keywords may be added using //request.setKeywords("Product Keywords");
+
+            //Category - a call to the ebay servers supports up to three unique categories
+            //Categories divided by integers, but are registered in the form of Strings
+            //reference category ID via http://www.isoldwhat.com/getcats/fullcategorytree.php?RootID=11232#11232
             request.getCategoryId().add("57988"); //Men's Clothing, Coats and Jackets
             request.getCategoryId().add("11484"); //Men's Clothing, Sweaters
-            //reference category ID via http://www.isoldwhat.com/getcats/fullcategorytree.php?RootID=11232#11232
-            // see https://forums.developer.ebay.com/questions/1208/how-to-limit-any-ebay-api-call-using-date-filter.html for item filters
-            
             
             //Detail Selection Using Aspect Filter
+            //Ebay's Aspect Filter describes the characteristics of an item that vary from category to category.
+            //For example, pants may be filtered by leg length and waist size, so it may have an aspect filter of
+            //leg length and waist size.  On the other hand, a book may be filtered by language or subject.  Therefore, 
+            //aspect filters the language of the book, as well as the subject of the book (e.x. action/mystery)
             //see http://expertland.net/question/s91qq5u5n2m61hx62asb7m37tmsz8o3/detail.html
+            
+            //A general aspect filter class must be created, which is then added to the request object.
             AspectFilter details = new AspectFilter();
             
             //Item Style
@@ -73,11 +73,12 @@ public class SearchListings {
             details.setAspectName("Size");
             details.getAspectValueName().add("M");
             details.getAspectValueName().add("L");
-            
             request.getAspectFilter().add(details);
             //
             
-            //Condition: new
+            //Item Condition
+            //EBay divides item condition typically via increments of 500 or 1000, with 1000 indicating that the 
+            //object is in the newest form, while subsequent numbers indicate incremental usage of the item.
             ItemFilter itemconditionfilter = new ItemFilter();
             itemconditionfilter.setName(ItemFilterType.CONDITION);
             List<String> itemcondition = itemconditionfilter.getValue();
@@ -98,70 +99,28 @@ public class SearchListings {
             duplicatefilter.setName(ItemFilterType.HIDE_DUPLICATE_ITEMS);
             duplicatefilter.getValue().add("true");
             request.getItemFilter().add(duplicatefilter);
-            //Duplicate Removal is not perfect, need to manually sort out most duplicates.
+            //Duplicate Removal is not perfect, will need to manually sort out most duplicates.
             
             //Sort Items by Newest First
             request.setSortOrder(SortOrderType.START_TIME_NEWEST);
             //
             
-            int searchentries = 30;
-            //Number of Returns per Search - setEntriesPerPage(int)
+            //Number of Returns per Search - the maximum limit of searches that can be returned is 100
             PaginationInput searches = new PaginationInput();
-            searches.setEntriesPerPage(searchentries); //Number of items found per search, max 100
-            request.setPaginationInput(searches);
+            searches.setEntriesPerPage(searchentries);
+            //Extra:  //see https://forums.developer.ebay.com/questions/1208/how-to-limit-any-ebay-api-call-using-date-filter.html for item filters
             //
             
             //Output Object
             FindItemsAdvancedResponse result = serviceClient.findItemsAdvanced(request);
             System.out.println("Found " + result.getSearchResult().getCount() + " relevant items." );
-            //
-            
-            
-            long firstid = 0;
-			int loopcount = 0;
-            long lastid = StoreInfo.checkSearched();
-            System.out.println(lastid);
-			StoreInfo.resetLastFound();
-			
-            SimpleDateFormat dateformat = new SimpleDateFormat("MM-dd-yyyy");
-            List<SearchItem> items = result.getSearchResult().getItem();
-            
-            for(SearchItem item : items) {
-            	//Currently does not support shipping costs due to NPE exceptions
-        		String brandname = SearchDetails.getRegisteredBrand(item.getTitle());
-        		long itemid = Long.parseLong(item.getItemId());
-        		String categorytype = item.getPrimaryCategory().getCategoryName();
-        		String listingdate = dateformat.format(item.getListingInfo().getStartTime().getTime());
-        		loopcount += 1;
-        		
-        		if(loopcount == 1){
-        			firstid = itemid;
-        		}
-        		
-        		if(brandname != "none" && !StoreInfo.checkDuplicate(itemid)){
-                	if(item.getListingInfo().isBuyItNowAvailable()){
-                		StoreInfo.insertData(itemid, brandname, categorytype, new BigDecimal(item.getListingInfo().getBuyItNowPrice().getValue()), listingdate);
-                	}
-	            	else{
-	            		StoreInfo.insertData(itemid, brandname, categorytype, new BigDecimal(item.getSellingStatus().getCurrentPrice().getValue()), listingdate);
-	            	}
-        		}
-        		
-        		if(loopcount == searchentries){
-        			StoreInfo.insertReference(firstid);
-        			System.out.println("Scanned through " + loopcount + " new items.");
-        		} 
-        		else if(lastid == itemid){
-        			StoreInfo.insertReference(firstid);
-        			System.out.println("Scanned through " + loopcount + " new items.");
-        			break;
-        		}
-	
-            }
-            
+            return result;
         } catch (Exception ex) {
         	// Handles Exceptions
             ex.printStackTrace();
         }
-    }
+		System.out.println("Something went horribly wrong!");
+		return null;
+	}
+
 }
