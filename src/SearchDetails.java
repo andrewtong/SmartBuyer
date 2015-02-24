1db413a46fdc0a3e9cfcb92f40951b16ef43690d
andrewtong <andrewtong19@gmail.com>
2015-02-23 23:39:42 -0800

    Update SearchDetails.java
    Redesigning of how categorizing names are indexed

--- src/SearchDetails.java
+++ src/SearchDetails.java
@@ -5,8 +5,17 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException
 
 public class SearchDetails {
+	
+	//File directory to store brand name (or any categorizing name) of the item to categorize listings by
+	public static File sbfile = new File("Complete Directory Listing e.x. C:\\Users\\Documents\\..etc");
+	
 	//This table contains the variables of interest.  For this particular project, it is focused on 
 	//designer brands.  The program will attempt to match the brand to the listing title and find a match.
 	private static final HashSet<String> designer = new HashSet<String>() {{
@@ -28,6 +37,35 @@ public class SearchDetails {
 		}
 		return "none";
 		
+	}
+	
+	public void checkFile(File file){
+		if(!file.exists()){
+			System.out.println("No reference file exists.");
+			return;
+		}
+		try {
+			HashCode crc32 = Files.hash(file, Hashing.crc32());
+			int cs = crc32.asInt();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		//if(Math.abs(cs) != getCurrentHash()){
+		//re-add listings into designer
+		//}
+		
+		try {
+            	Scanner input = new Scanner(file);
+            	while (input.hasNextLine()) {
+                	String line = input.nextLine();
+                	designer.add(line);
+            	}
+            	input.close();
+        	} catch (Exception ex) {
+            	ex.printStackTrace();
+        	}
 	}
 
 }
