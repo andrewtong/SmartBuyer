486a658fb16620dd5de785c289b8480aac303440
Andrew Tong <andrewtong19@gmail.com>
2015-04-19 01:25:01 -0700

    updated 4.19 parselistings.java

--- src/ParseListings.java
+++ src/ParseListings.java
@@ -3,16 +3,22 @@ package at.smartBuyer.parselistings;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.List;
-
 import com.ebay.services.finding.FindItemsAdvancedResponse;
-
 import at.smartBuyer.detailabstraction.SearchDetails;
+import at.smartBuyer.metadata.GenerateResults;
 import at.smartBuyer.searchlistings.SearchListings;
 import at.smartBuyer.sqlcommunication.StoreInfo;
 import at.smartBuyer.sqlcommunication.StoreInfo.ReferenceValues;
 import com.ebay.services.finding.SearchItem;
 
 public class ParseListings {
+	
+	String results;
+	
+	public static class ItemListingInfo{
+		public int marketfactor;
+		public int profitfactor;
+	}
 	
 	public static void parseSearchResults(List<SearchItem> items){	
 		
@@ -69,35 +75,57 @@ public class ParseListings {
 			if(loopcount == 1){
 				firstid = itemid;
 			}
-			
-			//If the item is a brand of interest and has not been registered before, it is then stored into
+
+    			//If the item is a brand of interest and has not been registered before, it is then stored into
     			//the SQL database.  (see at.smartBuyer.sqlcommunication.StoreInfo for functions)
-			if(brandname != "none" && !si.checkDuplicate(itemid)){
-	        	if(item.getListingInfo().isBuyItNowAvailable()){
-	        		si.insertData(itemid, brandname, categorytype, new BigDecimal(item.getListingInfo().getBuyItNowPrice().getValue()), listingdate);
-	        		itemsfound++;
-				}
-				else{
-					si.insertData(itemid, brandname, categorytype, new BigDecimal(item.getSellingStatus().getCurrentPrice().getValue()), listingdate);
-					itemsfound++;
-				}
-			}
+    			if(brandname != "none" && !si.checkDuplicate(itemid)){
+    				BigDecimal itemprice;
+            			if(item.getListingInfo().isBuyItNowAvailable()){
+            				itemprice = new BigDecimal(item.getListingInfo().getBuyItNowPrice().getValue());
+            			}
+            			else{
+            				itemprice = new BigDecimal(item.getSellingStatus().getCurrentPrice().getValue());
+            			}
+            			si.insertData(itemid, brandname, categorytype, itemprice, listingdate);
+            			itemsfound++;
+        			if(si.isLowPriced(brandname, itemprice)){
+        				ItemListingInfo currentitem = new ItemListingInfo();
+        				GenerateResults resultstoring = new GenerateResults();
+        				resultstoring.addResult(itemid, brandname, itemprice, currentitem.marketfactor, currentitem.profitfactor);
+        			}    			
+    			}
 			
-			//The program then proceeds to check whether the item has been scanned before.  The software stops once 
-			//it realizes it has scanned an item it has seen before, or if the list of search entries
-			//has been processed through.
-			if(loopcount == SearchListings.searchentries){
-				si.updateLastFound(firstid, crcvalue);
-				System.out.println("Scanned through " + loopcount + " new items.");
-				System.out.println("Added " + itemsfound + " new items to the database.");	
-			} 
-			else if(lastid == itemid){
-				si.updateLastFound(firstid, crcvalue);
-				System.out.println("Scanned through " + (loopcount-1) + " new items.");
-				System.out.println("Added " + itemsfound + " new items to the database.");	
-				break;
-			}
+    			//The program then proceeds to check whether the item has been scanned before.  The software stops once 
+    			//it realizes it has scanned an item it has seen before, or if the list of search entries
+    			//has been processed through.
+    			//Case where the algorithm has scanned through all of the new search entries.
+    			if(loopcount == SearchListings.searchentries){
+    				si.updateLastFound(firstid, crcvalue);
+    				System.out.println("Scanned through " + loopcount + " new items.");
+    				System.out.println("Added " + itemsfound + " new items to the database.");
+    				//This conditional can only occur once per run, so there is no repetition of updating cattable.
+    				if(loopcount > 0){
+    					si.updateCatTable();
+    				}
+    			} 
+    		
+    			//Case where the algorithm searches an item that was of the last search item of the previous run.
+    			else if(lastid == itemid){
+    				si.updateLastFound(firstid, crcvalue);
+    				System.out.println("Scanned through " + (loopcount-1) + " new items.");
+    				System.out.println("Added " + itemsfound + " new items to the database.");	
+    				if(loopcount-1 > 0){
+    					si.updateCatTable();
+    				}
+    				break;
+    			}
 		}
+	}
+	
+	public void getResultLog(){
+		GenerateResults extractlog = new GenerateResults();
+		System.out.println("Search Complete: ");
+		System.out.println(extractlog.retrieveResultLog());
 	}
 	
 	public static void main(String[] args) {
@@ -105,22 +133,28 @@ public class ParseListings {
 		FindItemsAdvancedResponse result = new FindItemsAdvancedResponse();
 		result = SearchListings.searchListings();
 		
+		StoreInfo search = new StoreInfo();
+		
 		//A connection to the SQL database then must be made in order to process and registered the incoming data.
-		StoreInfo.establishConnection();
+		search.establishConnection();
 		
 		//The listing table stores pricing information across brands of interest.
-		StoreInfo.lookupListingsTable();
-		
-		//The comparison table is used as a lookup to check up to what point can the software stop searching.
-		//For example, if the search has processed entries of id 1100 to 1000 (because it searches by newest), then
-		//searching anything older than or equal to id 1100 is redundant.
-		StoreInfo.lookupComparisonTable();
-		
-		StoreInfo.lookupCategoricalTable();
+        	search.lookupListingsTable();
         
-        	//The result object contains a list of items pertaining to what the user specified, and can be processed
-        	//through similarly how a normal list operates.
-		List<SearchItem> items = result.getSearchResult().getItem();
-        	parseSearchResults(items);
+        	//The comparison table is used as a lookup to check up to what point can the software stop searching.
+        	//For example, if the search has processed entries of id 1100 to 1000 (because it searches by newest), then
+        	//searching anything older than or equal to id 1100 is redundant.
+        	search.lookupComparisonTable();
+		
+        	search.lookupCategoricalTable(); 
+        
+		//The result object contains a list of items pertaining to what the user specified, and can be processed
+		//through similarly how a normal list operates.
+        	List<SearchItem> items = result.getSearchResult().getItem();
+        
+        	ParseListings pl = new ParseListings();
+        	pl.parseSearchResults(items);
+ 		pl.getResultLog();
+
 	}
 }
